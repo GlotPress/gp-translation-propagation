4476eaca44f15329339006ce16f6a497cc779764
Dominik Schilling <dominikschilling+git@gmail.com>
2016-03-20 21:21:13 +0100

    Tests: Add bootstrap files.

---
+++ tests/phpunit/bootstrap.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Determines where the GlotPress test suite lives.
+ */
+if ( false !== getenv( 'GP_TESTS_DIR' ) ) {
+	define( 'GP_TESTS_DIR', getenv( 'GP_TESTS_DIR' ) );
+} else {
+	define( 'GP_TESTS_DIR', dirname( dirname( dirname( __DIR__ ) ) ) . '/glotpress/tests/phpunit' );
+}
+
+if ( ! file_exists( GP_TESTS_DIR . '/bootstrap.php' ) ) {
+	die( "GlotPress test suite could not be found.\n" );
+}
+
+require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
+
+/**
+ * Load GlotPress and the plugin.
+ */
+function _bootstrap_plugins() {
+	require GP_TESTS_DIR . '/includes/loader.php';
+
+	require dirname( dirname( __DIR__ ) ) . '/gp-translation-propagation.php';
+}
+tests_add_filter( 'muplugins_loaded', '_bootstrap_plugins' );
+
+global $wp_tests_options;
+$wp_tests_options['permalink_structure'] = '/%postname%';
+
+require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
+
+require GP_TESTS_DIR . '/lib/testcase.php';
---
+++ tests/phpunit/tests/test-class-gp-translation-propagation.php
@@ -0,0 +1,8 @@
+<?php
+
+class Test_GP_Translation_Propagation extends GP_UnitTestCase {
+
+	function tests_tests() {
+		$this->assertTrue( true );
+	}
+}
